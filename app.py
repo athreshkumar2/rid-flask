b1ddae31841077b4992bd67e35a83b91ac10c228
athresh <athreshkumar2@gmail.com>
2021-11-18 03:44:17 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+    # return "<p>Hello, World!</p>"
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+
+@app.route("/details")
+def details():
+    return render_template('details.html')
+
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template('portfolio.html')
+
+
+@app.route("/projects")
+def projects():
+    return render_template('projects.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
